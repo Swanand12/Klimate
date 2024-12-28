176fce585f4a17404ab1245c44e60276bf7ebfd6
Swanand12 <sawantswanand2004@gmail.com>
2024-12-28 13:56:34 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { Link, useLocation, useNavigate } from 'react-router-dom'
 import SearchCity from './SearchCity'
 import { Button } from './ui/button'
 import { IoMdArrowRoundBack } from "react-icons/io";
+import logo from "../assets/logo-2.svg"
 
 const Header = () => {
   const location = useLocation()
@@ -10,7 +11,7 @@ const Header = () => {
 
   return (
     <header className={`flex justify-between items-center py-4  px-[5rem] bg-black `}>
-        <Link to="/"><img className='h-14 ' src='../src/assets/logo-2.png' alt='logo'/></Link>
+        <Link to="/"><img className='h-14 ' src={logo} alt='logo'/></Link>
         {location.pathname === "/" ? <SearchCity/> : <button onClick={()=>navigate("/")} className='text-white bg-black p-2 rounded-lg border border-gray-500'><IoMdArrowRoundBack strokeWidth={1} size={25}/></button>}
     </header>
   )
