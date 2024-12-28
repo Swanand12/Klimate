b2df25c0127b48728130a94ed46a594a694851cd
Swanand12 <sawantswanand2004@gmail.com>
2024-12-28 12:37:34 +0530

    adjust the styling of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='border-t bg-black bg-opacity-60 h-[7rem] text-gray-400 flex justify-center items-center '>
+    <footer className=' bg-black bg-opacity-60 h-[7rem] text-gray-400 flex justify-center items-center '>
         Made By Swanand Sawant
     </footer>
   )
